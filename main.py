8b39cd9c8898b2d2abc184f0b9840310928d4e35
relf108 <tristan.sutton@gmail.com>
2023-06-09 18:16:04 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from core import auth
+import uvicorn
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from redis import asyncio as aioredis
+
+app = FastAPI()
+
+app.include_router(auth.router)
+
+@cache()
+async def get_cache():
+    return 1
+
+
+@app.get("/")
+@cache(expire=60)
+async def index():
+    return dict(hello="world")
+
+
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
+def main():
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+if __name__ == "__main__":
+    main()
